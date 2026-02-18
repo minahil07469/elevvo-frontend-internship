ead0d15806b470572fac8290454f43d7dbeecc3e
minahil07469 <minahilmunnir07@gmail.com>
2026-02-18 06:54:22 -0800

    Delete Elevvo-Internship directory

--- Elevvo-Internship/Level1-Task1-sidebar/task1.js
+++
@@ -1,5 +0,0 @@
-  const menu_btn = document.querySelector("#menu-toggle")
-        const sideBar_container = document.querySelector(".side-bar");
-        menu_btn.addEventListener("click", (() => {
-            sideBar_container.classList.toggle("active");
-        }))

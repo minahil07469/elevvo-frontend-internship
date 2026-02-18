cbc0894f0490d7ae77762cf4735cb2d313ebf94a
minahil07469 <minahilmunnir07@gmail.com>
2026-02-18 07:04:48 -0800

    Add files via upload

---
+++ Elevvo-Internship/Level1-Task2-ContactForm/scripts.js
@@ -0,0 +1,26 @@
+const form = document.querySelector(".form");
+const form_container = document.querySelector(".form-container");
+const nameInput = document.querySelector(".name-input");
+const emailInput = document.querySelector(".email-input");
+const subInput = document.querySelector(".sub-input");
+const login = document.querySelector(".login");
+const textArea=document.querySelector("#text-box")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    submitForm();
+});
+
+function submitForm() {
+    if (nameInput.value === '' || emailInput.value === '' || subInput.value === '') {
+        return;
+    }
+
+    alert("Form submitted successfully 🎉")
+    nameInput.value = '';
+    emailInput.value = '';
+    subInput.value = '';
+    textArea.value='';
+}
+
+
